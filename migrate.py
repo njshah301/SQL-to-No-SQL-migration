25884ff1e34125de6c718ac9f79363655d13849c
Aksh Patel <201901005@daiict.ac.in>
2022-06-15 23:27:57 +0530

    added new features

--- migrate.py
+++ migrate.py
@@ -1,22 +1,23 @@
 import psycopg2
+import psycopg2.extras
 import pymongo
 import ssl
+
 from bson.json_util import dumps, loads
 import datetime
-
-
 
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
-
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+delete_existing_documents = True
 
 begin_time = datetime.datetime.now()
 print(f"{bcolors.HEADER}Script started at: {begin_time} {bcolors.ENDC}")
@@ -25,96 +26,111 @@ print(f"{bcolors.HEADER}Initializing database connections...{bcolors.ENDC}")
 #MySQL connection
 print(f"{bcolors.HEADER}Connecting to PostgreSQL server...{bcolors.ENDC}")
 
-pgsqldb = psycopg2.connect(
-   database="Basketball", user='postgres', password='admin', host='127.0.0.1', port= '5432'
-)
+pgsqldb = psycopg2.connect(host="localhost",database="Entertainment_Booking_System",user="postgres",password="admin")
 
 print(f"{bcolors.HEADER}Connection to MongoDB Server succeeded.{bcolors.ENDC}")
 
-
 cursor = pgsqldb.cursor()
-cursor.execute("select t.table_name,array_agg(c.column_name::text) as columns from information_schema.tables t inner join information_schema.columns c on t.table_name = c.table_name where t.table_schema = 'Sports_Training' and t.table_type= 'BASE TABLE'  and c.table_schema = 'Sports_Training' group by t.table_name; ")
-# cursor.execute('''SELECT * FROM "Sports_Training"."Admin";''')
-myresult = (cursor.fetchall())
-print(myresult)
+
+# # Query 1
+# ch="select * from main_db.manager order by m_id"
+# print(ch)
+# cursor.execute(ch)
+# rows=cursor.fetchall()
+# for r in rows:
+#     print(r[0],r[1],r[2],r[3])
 
 print(f"{bcolors.HEADER}Connecting to MongoDB server...{bcolors.ENDC}")
 
-mongodb_host = "mongodb+srv://njshah301:*NILAy4564*@cluster0.lyugc.mongodb.net/test"
-mongodb_dbname = "mymongodb"
+mongodb_host = "mongodb+srv://<id>:<password>@cluster0.juxn7.mongodb.net/test"
+mongodb_dbname = "pg2mongo"
 
-
-myclient = pymongo.MongoClient(mongodb_host,tls=True, tlsAllowInvalidCertificates=True)
-
-
+myclient = pymongo.MongoClient(mongodb_host)
+mydb = myclient[mongodb_dbname]
 
 print(f"{bcolors.HEADER}Connection to MongoDB Server succeeded.{bcolors.ENDC}")
+print(f"{bcolors.HEADER}Database connections initialized successfully.{bcolors.ENDC}")   
 
-print(f"{bcolors.HEADER}Database connections initialized successfully.{bcolors.ENDC}")  
-mydb=myclient["lab08"]
-mycol=mydb["movies"]
-
-query={"directors": {"$in": [ "King Vidor"]}}
-
-x=mycol.find(query,{"title": 1, "_id":0})
-print('Title of movies in which one of director is “King Vidor”')
-for temp in x:
-    print(temp)
 #Start migration
+print(f"Migration started...")
+dblist = myclient.list_database_names()
+print(dblist)
+if mongodb_dbname in dblist:
+    print(f"The database exists.")
+else:
+    print(f"The database does not exist, it is being created.")  
 
-# print(f"{bcolors.HEADER}Migration started...{bcolors.ENDC}")
 
+#Iterate through the list of tables in the schema
+cursor = pgsqldb.cursor()
+cursor.execute("""SELECT table_schema, table_name
+                      FROM information_schema.tables
+                      WHERE table_schema != 'pg_catalog' AND table_schema != 'information_schema' AND table_type='BASE TABLE'
+                      ORDER BY table_schema, table_name""")
 
-# if mongodb_dbname in dblist:
-#     print(f"{bcolors.OKBLUE}The database exists.{bcolors.ENDC}")
-# else:
-#     print(f"{bcolors.WARNING}The database does not exist, it is being created.{bcolors.ENDC}")  
+tables = cursor.fetchall()
+for t_name_table in tables:
+    print(t_name_table)
+print("------------------------------------------")
+
+for t in tables:
+    where_dict = {"table_schema": t[0], "table_name": t[1]}
+    cursor.execute("""SELECT column_name, ordinal_position, is_nullable, data_type, character_maximum_length
+                      FROM information_schema.columns
+                      WHERE table_schema = %(table_schema)s
+                      AND table_name   = %(table_name)s
+                      ORDER BY ordinal_position""", where_dict)
+    cols = cursor.fetchall()
+
+    for c in cols:
+        print(c)
+    print("---------------------------------------------")
 
 #Function migrate_table 
-# def migrate_table(db, col_name):
-#     mycursor = db.cursor(dictionary=True)
-#     mycursor.execute("SELECT * FROM " + col_name + ";")
-#     myresult = mycursor.fetchall()
+def migrate_table(db, table_name, table_schema):
+    cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM " + table_schema + "." + table_name + ";")
+    myresult = cursor.fetchall()
+    # print(myresult)
+    resDict = []
+    for row in myresult:
+        resDict.append(dict(row))
+    # print(resDict)
 
-#     mycol = mydb[col_name]
-    
-#     if delete_existing_documents:
-#         #delete all documents in the collection
-#         mycol.delete_many({})
+    mycol = mydb[table_name]
+    if delete_existing_documents:
+        #delete all documents in the collection
+        mycol.delete_many({})
 
-#     #insert the documents
-#     if len(myresult) > 0:
-#         x = mycol.insert_many(myresult)
-#         return len(x.inserted_ids)
-#     else:
-#         return 0
+    #insert the documents
+    if len(myresult) > 0:
+        x = mycol.insert_many(resDict)
+        return len(x.inserted_ids)
+    else:
+        return 0
 
-#     #Iterate through the list of tables in the schema
-# table_list_cursor = pgsqldb.cursor()
-# table_list_cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = %s ORDER BY table_name LIMIT 15;", (mysql_schema,))
-# tables = table_list_cursor.fetchall()
+total_count = len(tables)
+success_count = 0
+fail_count = 0
 
-# total_count = len(tables)
-# success_count = 0
-# fail_count = 0
-
-# for table in tables:
-#     try:
-#         print(f"{bcolors.OKCYAN}Processing table: {table[0]}...{bcolors.ENDC}")
-#         inserted_count = migrate_table(pgsqldb, table[0])
-#         print(f"{bcolors.OKGREEN}Processing table: {table[0]} completed. {inserted_count} documents inserted.{bcolors.ENDC}")
-#         success_count += 1
-#     except Exception as e:
-
-#         print(f"{bcolors.FAIL} {e} {bcolors.ENDC}")
-#         fail_count += 1
+for table in tables:
+    try:
+        print(f"{bcolors.OKCYAN}Processing table: {table[1]}...{bcolors.ENDC}")
+        inserted_count = migrate_table(pgsqldb, table[1], table[0])
+        print(f"{bcolors.OKGREEN}Processing table: {table[1]} completed. {inserted_count} documents inserted.{bcolors.ENDC}")
+        success_count += 1
+    except Exception as e:
+        print(f"{bcolors.FAIL} {e} {bcolors.ENDC}")
+        fail_count += 1
         
-# print("")
-# print("Migration completed.")
-# print(f"{bcolors.OKGREEN}{success_count} of {total_count} tables migrated successfully.{bcolors.ENDC}")
-# if fail_count > 0:
-#     print(f"{bcolors.FAIL}Migration of {fail_count} tables failed. See errors above.{bcolors.ENDC}")
+print("")
+print("Migration completed.")
 
-# end_time = datetime.datetime.now()
-# print(f"{bcolors.HEADER}Script completed at: {end_time} {bcolors.ENDC}")
-# print(f"{bcolors.HEADER}Total execution time: {end_time-begin_time} {bcolors.ENDC}")
+print(f"{bcolors.OKGREEN}{success_count} of {total_count} tables migrated successfully.{bcolors.ENDC}")
+
+if fail_count > 0:
+    print(f"{bcolors.FAIL}Migration of {fail_count} tables failed. See errors above.{bcolors.ENDC}")
+
+end_time = datetime.datetime.now()
+print(f"{bcolors.HEADER}Script completed at: {end_time} {bcolors.ENDC}")
+print(f"{bcolors.HEADER}Total execution time: {end_time-begin_time} {bcolors.ENDC}")
